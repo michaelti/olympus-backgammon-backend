8e4bc9aef515fbbacae3ade0dfd696baa2f5e4c6
Blaise <blaise@live.ca>
2020-05-18 18:32:50 -0400

    Added Player Enum; added turn to Board object
    * Replaced string with pseudo-Enum for storing player identifier
    * Added who’s turn it is to the game board object
    * Removed commented out code

--- plakoto.js
+++ plakoto.js
@@ -1,15 +1,20 @@
 const prompt = require('prompt-sync')();
 
-//const colour = Object.freeze({"empty":0, "black":1, "white":2});
+// Enum-like object
+const Player = Object.freeze({
+  black : {value: -1, name: "Black", code: "B"}, 
+  empty : {value: 0,  name: "Empty", code: "-"}, 
+  white : {value: 1,  name: "White", code: "W"}
+});
+
 let Pip = {
 	size: 0,
-	top: "-",//colour.empty,
-	bot: "-",//colour.empty,
-	print() {
-		console.log(this.size + 1);
-	}
+	top: Player.empty,
+	bot: Player.empty,
 };
+
 let Board = {
+	turn: Player.black,
 	off1: 0, off2: 0,
 	bar1: 0, bar2: 0,
 	pips: new Array(25),
@@ -19,9 +24,9 @@ let Board = {
 		for (i=0; i<=24; i++) {
 			this.pips[i] = Object.create(Pip);
 		}
-		this.pips[1].top = this.pips[1].bot = "W"; 
+		this.pips[1].top = this.pips[1].bot = Player.white; 
 		this.pips[1].size = 15;
-		this.pips[24].top = this.pips[24].bot = "B"; 
+		this.pips[24].top = this.pips[24].bot = Player.black; 
 		this.pips[24].size = 15;
 	},
 
@@ -29,15 +34,15 @@ let Board = {
 	print() {
 		let temp = "";
 		for (i=13; i<=24; i++) {
-			temp += `(${ i })${ this.pips[i].top }${ this.pips[i].size }${ this.pips[i].bot } `;
+			temp += `(${ i })${ this.pips[i].top.code }${ this.pips[i].size }${ this.pips[i].bot.code } `;
 		}
 		console.log(temp);
 		temp = "";
 		for (i=12; i>=1; i--) {
 			if (i < 10)
-				temp += `( ${ i })${ this.pips[i].top }${ this.pips[i].size }${ this.pips[i].bot } `;
+				temp += `( ${ i })${ this.pips[i].top.code }${ this.pips[i].size }${ this.pips[i].bot.code } `;
 			else
-				temp += `(${ i })${ this.pips[i].top }${ this.pips[i].size }${ this.pips[i].bot } `;
+				temp += `(${ i })${ this.pips[i].top.code }${ this.pips[i].size }${ this.pips[i].bot.code } `;
 		}
 		console.log(temp + '\n');
 	},
@@ -47,31 +52,31 @@ let Board = {
 	// from: 	Move from pip # <eg. 1>
 	// to: 		Move to pip # <eg. 4>
 	// return: 	Returns a boolean
-	isValid(turn, from, to) {
-		if (turn === "black") {
-			if (this.pips[from].top !== "B") {
+	isValid(from, to) {
+		if (this.turn === Player.black) {
+			if (this.pips[from].top !== Player.black) {
 				return false;
 			}
-			if (this.pips[to].top === "W" && this.pips[to].size > 1) {
+			if (this.pips[to].top === Player.white && this.pips[to].size > 1) {
 				return false;
 			}
 		}
-		else if (turn === "white") {
-			if (this.pips[from].top !== "W") {
+		else if (this.turn === Player.white) {
+			if (this.pips[from].top !== Player.white) {
 				return false;
 			}
-			if (this.pips[to].top === "B" && this.pips[to].size > 1) {
+			if (this.pips[to].top === Player.black && this.pips[to].size > 1) {
 				return false;
 			}
 		}
 		return true;
 	},
 
-	doMove(turn, from, to) {
+	doMove(from, to) {
 		// From pip
 		if (this.pips[from].size === 1) {
-			this.pips[from].top = '-';
-			this.pips[from].bot = '-';
+			this.pips[from].top = Player.empty;
+			this.pips[from].bot = Player.empty;
 		}
 		else if (this.pips[from].size === 2 && this.pips[from].top !== this.pips[from].bot){
 			this.pips[from].top = this.pips[from].bot;
@@ -80,16 +85,15 @@ let Board = {
 
 		// To pip
 		if (this.pips[to].size === 0) {
-			this.pips[to].bot = turn;
+			this.pips[to].bot = this.turn;
 		}
-		this.pips[to].top = turn;
+		this.pips[to].top = this.turn;
 		this.pips[to].size ++;
 	}
 };
 
 var board1 = Object.create(Board);
 board1.initPlakoto();
-//board1.pips[13].top = "black";
 board1.print();
 let from;
 let to;
@@ -99,17 +103,18 @@ while (true) {
 		from = prompt('Black move from: ');
 		  to = prompt('Black move to  : ');
 	} 
-	while (! board1.isValid("black", from, to));
+	while (! board1.isValid(from, to));
 
-	board1.doMove("B", from, to);
+	board1.doMove(from, to);
 	board1.print();
-
+	board1.turn = Player.white;
 	do {
 		from = prompt('White move from: ');
 		  to = prompt('White move to  : ');
 	} 
-	while (! board1.isValid("white", from, to));
+	while (! board1.isValid(from, to));
 
-	board1.doMove("W", from, to);
+	board1.doMove(from, to);
 	board1.print();
+	board1.turn = Player.black;
 }
