a54fd531b806c2bcb113d7fc9e489add602ee177
Blaise <blaise@live.ca>
2020-05-20 18:31:16 -0400

    Added dice; changed function constructor for Pip object

--- plakoto.js
+++ plakoto.js
@@ -2,15 +2,15 @@ const prompt = require('prompt-sync')();
 
 // Enum-like object
 const Player = Object.freeze({
-  empty : {value: 0, name: "Empty", code: "-"},
-  white : {value: 1, name: "White", code: "W"},
-  black : {value: 2, name: "Black", code: "B"},
+  empty : {direction: 0, name: "Empty", code: "-"},
+  white : {direction: 1, name: "White", code: "W"},
+  black : {direction:-1, name: "Black", code: "B"},
 });
 
-let Pip = {
-	size: 0,
-	top: Player.empty,
-	bot: Player.empty,
+function Pip(size = 0, owner = Player.empty) {
+	this.size = size;
+	this.top = owner;
+	this.bot = owner;
 };
 
 let Board = {
@@ -18,17 +18,24 @@ let Board = {
 	off1: 0, off2: 0,
 	bar1: 0, bar2: 0,
 	pips: new Array(25),
+	diceValue: new Array(2),
 
 	// Initialize the board for a game of plakoto
 	initPlakoto() {
 		this.turn = Player.black;	// Later, players will roll to see who goes first
+		this.rollDice();
 		for (i=0; i<=24; i++) {
-			this.pips[i] = Object.create(Pip);
+			this.pips[i] = new Pip();
 		}
-		this.pips[1].top = this.pips[1].bot = Player.white; 
-		this.pips[1].size = 15;
-		this.pips[24].top = this.pips[24].bot = Player.black; 
-		this.pips[24].size = 15;
+		this.pips[24] = new Pip(15, Player.black);	// Black moves towards pip 1 (decreasing)
+		this.pips[1] = new Pip(15, Player.white);	// White moves towards pip 24 (increasing)
+	},
+
+	rollDice() {
+  		this.diceValue = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
+  		if (this.diceValue[0] === this.diceValue[1]) {
+  			this.diceValue = this.diceValue.concat(this.diceValue);
+  		}
 	},
 
 	// Print an ASCII game board to console
@@ -59,6 +66,9 @@ let Board = {
 		if (this.pips[to].top === this.otherPlayer() && this.pips[to].size > 1) {
 			return false;
 		}
+		if (! this.diceValue.includes( this.turn.direction * (to - from) ) ) {
+			return false;
+		}
 		return true;
 	},
 
@@ -79,6 +89,13 @@ let Board = {
 		}
 		this.pips[to].top = this.turn;
 		this.pips[to].size ++;
+
+		// Handle dice. NOTE: this will only work for 2 distinct values or 4 idencital values
+		if (this.diceValue[0] === Math.abs(from - to)) {
+			this.diceValue.shift();
+		} else {
+			this.diceValue.pop();
+		}
 	},
 
 	// Returns the player who's turn it ISN'T
@@ -96,13 +113,15 @@ let from;
 let to;
 
 while (true) {
-	do {
+	while (board.diceValue.length > 0) {
+		console.log("Your dice are: " + board.diceValue);
 		from = prompt(`${board.turn.name} move from: `);
-		  to = prompt(`${board.turn.name} move to  : `);
-	} 
-	while (! board.isValid(from, to));
-
-	board.doMove(from, to);
-	board.print();
+		to   = prompt(`${board.turn.name} move to  : `);
+		if (board.isValid(from, to)) {
+			board.doMove(from, to);
+			board.print();
+		}
+	}
 	board.turn = board.otherPlayer();
+	board.rollDice();
 }
