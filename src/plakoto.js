9a523af116996adb6f133571e338c402fe006e32
Michael <m@michaelti.ca>
2020-07-11 19:57:05 -0400

    Add winner property

--- src/plakoto.js
+++ src/plakoto.js
@@ -14,8 +14,8 @@ const Plakoto = () => ({
         for (let i = 0; i <= 24; i++) {
             this.pips[i] = Pip();
         }
-        this.pips[24] = Pip(15, Player.black); // Black moves towards pip 1 (decreasing)
-        this.pips[1] = Pip(15, Player.white); // White moves towards pip 24 (increasing)
+        this.pips[1] = Pip(15, Player.black); // Black moves towards pip 1 (decreasing)
+        this.pips[24] = Pip(15, Player.white); // White moves towards pip 24 (increasing)
     },
 
     // Is the move valid?
