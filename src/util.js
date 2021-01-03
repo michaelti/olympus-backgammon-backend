1a81bc6d1c85e5910fc5a0fd621ae5059dc44922
Blaise <blaise@live.ca>
2021-01-03 16:42:39 -0500

    Simplify random numeric generator

--- src/util.js
+++ src/util.js
@@ -1,13 +1,7 @@
 // Return a random numeric string of length n
 exports.randomNumeric = (length) => {
-    const chars = "0123456789";
-    let result = "";
-
-    for (let i = 0; i < length; i++) {
-        result += chars[Math.floor(Math.random() * chars.length)];
-    }
-
-    return result;
+    const num = Math.floor(Math.random() * 10 ** length);
+    return num.toString().padStart(length, 0);
 };
 
 exports.range = (start, end, length = end - start + 1) =>
