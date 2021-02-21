4ab92854049999364a83f658fb7917aa7986a3cf
Michael <m@michaelti.ca>
2021-02-21 18:46:24 -0500

    Add intelligence to the bot

--- src/bot.js
+++ src/bot.js
@@ -12,6 +12,71 @@ const cloneBoard = {
     [Variant.fevga]: (boardState) => ({ ...fevga.Board(), ...clone(boardState) }),
 };
 
+function pickTurn(turns, board) {
+    const { turn } = turns.reduce(
+        (bestTurn, currentTurn) => {
+            const clonedBoard = clone(board);
+
+            currentTurn.forEach((move) => {
+                clonedBoard.doMove(move.from, move.to);
+            });
+
+            const rank = rankBoard(clonedBoard);
+
+            if (rank > bestTurn.rank) return { turn: currentTurn, rank };
+            return bestTurn;
+        },
+        { turn: [], rank: -Infinity }
+    );
+
+    return turn;
+}
+
+function rankBoard(board) {
+    let rank = 0;
+
+    // -10 = very bad
+    // -5 = kinda bad
+    // 0 = neutral
+    // +5 = kinda good
+    // +10 = very good
+
+    let isEndGame = true;
+    let sawBlack = false;
+
+    for (let i = 25; i >= 0; i--) {
+        if (board.pips[i].top === -1) sawBlack = true;
+        if (board.pips[i].top === 1 && sawBlack) {
+            isEndGame = false;
+            break;
+        }
+    }
+
+    board.pips.forEach((pip, i) => {
+        // An open checker
+        if (pip.top === board.turn && pip.size === 1) {
+            for (let j = i - 1; j >= 0; j--) {
+                // If a white checker exists ahead
+                if (board.pips[j].top === 1 && board.pips[j].size > 0) {
+                    rank -= 24 - i;
+                    break;
+                }
+            }
+        }
+
+        // A closed door while not in the endgame
+        if (pip.top === board.turn && pip.size > 1 && !isEndGame) rank += 5;
+    });
+
+    // If we send the opponent to the bar
+    rank += board.pips[0].size * 20;
+
+    // If we bear off
+    rank += board.off[-1] * 20;
+
+    return rank;
+}
+
 const bot = () => {
     const socket = io(`http://localhost:3001`);
     let roomLocal = {};
@@ -35,10 +100,6 @@ const bot = () => {
                 console.log("Bot moving");
                 doingMove = true;
 
-                // Todo:
-                // - Get it to play a good turn instead of a random one
-                // - Clean it up
-
                 const logicBoard = cloneBoard[roomLocal.variant](roomLocal.board);
 
                 logicBoard.maxTurnLength = 0;
@@ -53,7 +114,21 @@ const bot = () => {
                     return logicBoard.turnValidator(turn) > 0;
                 });
 
-                const turn = onlyValidTurns[Math.floor(Math.random() * onlyValidTurns.length)];
+                const turn = pickTurn(onlyValidTurns, logicBoard);
+
+                // IDEA 1: dedupe the unique turns
+                // const strings = onlyValidTurns.map((turn) => {
+                //     const clonedBoard = clone(logicBoard);
+
+                //     turn.forEach((move) => {
+                //         clonedBoard.doMove(move.from, move.to);
+                //     });
+
+                //     return JSON.stringify(clonedBoard.pips);
+                // });
+
+                // const uniqueTurns = new Set(strings);
+                // console.log(uniqueTurns.size);
 
                 turn.forEach(async (move, i) => {
                     await think(750 * (i + 1));
@@ -64,7 +139,7 @@ const bot = () => {
             // Finish a turn
             if (roomLocal.board.turnValidity > 0) {
                 console.log("Bot applying turn");
-                socket.emit("game/apply-turn");
+                // socket.emit("game/apply-turn");
                 doingMove = false;
             }
         }
